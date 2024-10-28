d8e95f8bacc395d7bb3c0e2710fd5a1be42a23b1
WIN-OPM3TB4AK0M\ICT02_19 <asdf@gmail.com>
2024-10-28 10:32:44 +0900

    stream, :: 문법

--- java_lamda/src/StreamMain2.java
+++ java_lamda/src/StreamMain2.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class StreamMain2 {
 
@@ -10,11 +12,16 @@ public class StreamMain2 {
 		sArr.add(new Student(80, 60, 70, "홍길순"));
 		sArr.add(new Student(70, 60, 70, "홍말자"));
 		sArr.add(new Student(50, 50, 50, "바둑이"));
-		
-		sArr.stream()
-			.filter(student -> student.name.startsWith("홍"))
-			.mapToDouble(student -> (student.kor + student.eng + student.math) / 3.0)
-			.forEach(avg -> System.out.println(avg));
 
+		sArr.stream().filter(student -> student.name.startsWith("홍"))
+				.mapToDouble(student -> (student.kor + student.eng + student.math) / 3.0)
+				.forEach(avg -> System.out.println(avg));
+
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+		//Optional<Integer> sum = numbers.stream().reduce((x, y) -> x + y);
+		Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
+		
+		sum.ifPresent(s -> System.out.println("sum = " + s));
 	}
 }
